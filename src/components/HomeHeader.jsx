d42c93b0846762ea2a51fb45ca73e3c64b5d838a
ragini12-official <ragininevaskar.official7@gmail.com>
2024-04-08 19:31:48 +0530

    Added body section in the home page

--- src/components/HomeHeader.jsx
+++ src/components/HomeHeader.jsx
@@ -15,7 +15,7 @@ export default function HomeHeader() {
 
                 <TbGridDots className='bg-transparent hover:bg-gray-200 rounded-full text-4xl p-2'/>
 
-                <button className='bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md transition-shadow'>Sign In</button>
+                <button className='bg-blue-600 text-white px-6 py-2 font-medium rounded-md hover:brightness-90 hover:shadow-md transition-shadow'>Sign In</button>
             </div>
         </header>
     )
---
+++ src/components/HomeSearch.jsx
@@ -0,0 +1,67 @@
+'use client'
+
+import React, { useState } from 'react';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { BsFillMicFill } from 'react-icons/bs';
+
+import { useRouter } from 'next/navigation';
+
+export default function HomeSearch() {
+
+  const [input, setInput] = useState('');
+  const [randomSearchLoader, setRandomSearchLoader] = useState(false);
+  const router = useRouter();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!input.trim()) return;
+    router.push(`/search/web?searchTerm=${input}`);
+  }
+
+  const randomSearch = async (e) => {
+    setRandomSearchLoader(true);
+    console.log("randomSearch called...");
+    const response = await fetch(`https://random-word-api.herokuapp.com/word`)
+      .then((res) => res.json())
+      .then((data) => data[0]);
+    if(!response) return;
+    router.push(`/search/web?searchTerm=${response}`);
+    setRandomSearchLoader(false);
+    // console.log(response);
+  }
+
+  return (
+    <>
+      <form
+        // onSubmit={handleSubmit}
+        className='flex w-full mt-5 mx-auto max-w-{90%} 
+      border border-gray-200 px-5 py-3 rounded-full 
+      hover:shadow-md focus:shadow-md transition-shadow 
+      sm:max-w-xl lg:max-w-2xl'>
+        <AiOutlineSearch
+          className='text-xl text-gray-700 mr-3' />
+        <input
+          type='text'
+          placeholder='Search Google or type a URL'
+          className='flex-grow focus:outline-none '
+          onChange={e => setInput(e.target.value)} />
+        <BsFillMicFill className='text-lg ' />
+      </form>
+
+      <div className='flex flex-col space-y-2 sm:space-y-0 justify-center sm:flex-row mt-6 sm:space-x-4'>
+        <button
+          onClick={handleSubmit}
+          type='submit'
+          className='bg-[#f8f9fa] rounded-md text-sm text-gray-800 hover:ring-gray-200 focus:outline-none active:ring-gray-300 hover:shadow-md w-36 h-10 transition-shadow'>
+          Google Search
+        </button>
+        <button
+          onClick={randomSearch}
+          disabled={randomSearchLoader}
+          className='bg-[#f8f9fa] rounded-md text-sm text-gray-800 hover:ring-gray-200 focus:outline-none active:ring-gray-300 hover:shadow-md w-36 h-10 transition-shadow disabled:opacity-75 disabled:shadow-sm'>
+          {randomSearchLoader ? "Loading..." : "I'm Feeling Lucky"}
+        </button>
+      </div>
+    </>
+  )
+}
