1aa81b50635b17332307abfe9b082661f0f216cb
ragini12-official <ragininevaskar.official7@gmail.com>
2024-04-09 17:21:24 +0530

    Completed the SearchBox component in the header in the Search module

--- src/components/SearchModule_Search.jsx
+++ src/components/SearchModule_Search.jsx
@@ -1,11 +1,33 @@
 'use client'
 
-import React from 'react'
+import React, { useState } from 'react';
+import { useSearchParams, useRouter } from 'next/navigation';
+
+import { RxCross2 } from 'react-icons/rx';
+import { BsFillMicFill } from 'react-icons/bs';
+import { AiOutlineSearch } from 'react-icons/ai';
 
 export default function SearchBox() {
+    const router = useRouter();
+    const searchParams = useSearchParams();
+    const searchTerm = searchParams.get('searchTerm');
+    const [query, setQuery] = useState(searchTerm || '');
+
+    function handleSubmit (e) {
+        e.preventDefault();
+        if(!query.trim) {
+            console.log("Empty searchTerm");
+            return;
+        }
+        router.push(`/search/web?searchTerm=${query}`);
+    }
+
   return (
-    <div className='border'>
-        SearchBox
-    </div>
+    <form onSubmit={handleSubmit} className='flex border border-gray-200 rounded-full shadow-lg px-6 py-3 ml-10 mr-5 flex-grow max-w-3xl items-center justify-around'>
+        <input type='text' onChange={(e) => setQuery(e.target.value)} value={query} className='w-full focus:outline-none'/>
+        <RxCross2 onClick={e => setQuery('')} className='text-2xl text-gray-500 cursor-pointer sm:mr-2'/>
+        <BsFillMicFill className='hidden sm:inline-flex text-3xl text-blue-500 border-l-2 pl-2 border-gray-300 mr-3'/>
+        <AiOutlineSearch onClick={handleSubmit} className='text-2xl hidden sm:inline-flex text-blue-500 cursor-pointer'/>
+    </form>
   )
 }
