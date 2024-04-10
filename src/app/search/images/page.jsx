a5a2206be77e6269481ce042fcada2430ad960e2
ragini12-official <ragininevaskar.official7@gmail.com>
2024-04-10 19:32:04 +0530

    Added image search results

--- src/app/search/images/page.jsx
+++ src/app/search/images/page.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React from 'react';
 
-export default function ImageSearchPage() {
-  return (
-    <div>ImageSearchPage</div>
-  )
+import ImageSearchResults from '@/components/ImageSearchResults';
+
+export default async function ImageSearchPage({ searchParams }) {
+
+    const query = searchParams['searchTerm'];
+    const API_URL = `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${query}&searchType=image`;
+
+    const response = await fetch(API_URL);
+    if(!response || !response.ok) {
+        throw new Error("Could not fetch data from the API.");
+    }
+    const data = await response.json();
+    const results = data.items;
+
+    // const results = await fetch(API_URL)
+    //     .then((res) => res.json())
+    //     .then((data) => data.items);
+
+    if(!results) {
+        throw new Error("No results found!");
+    }
+
+    return (
+        <div className=''>
+            <ImageSearchResults results={data}/>
+        </div>
+    )
 }
